aea693b4bf223f8b1a980228c74b9b07f233e2c6
Mykyta Grogul <mykyta006@gmail.com>
2025-08-05 11:05:46 +0200

    UI Click and Drag events

--- The Eternal Curse/Assets/Scripts/UI/InventoryItem.cs
+++ The Eternal Curse/Assets/Scripts/UI/InventoryItem.cs
@@ -1,16 +1,123 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.EventSystems;
 
-public class InventoryItem : MonoBehaviour
+public class InventoryItem : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDropHandler, IEndDragHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private Image itemImage;
+
+    [SerializeField]
+    private TMP_Text quantityTxt;
+
+    [SerializeField]
+    private Image borderImage;
+
+    public event Action<InventoryItem> OnItemClicked;
+    public event Action<InventoryItem> OnItemDroppedOn;
+    public event Action<InventoryItem> OnItemBeginDrag;
+    public event Action<InventoryItem> OnItemEndDrag;
+    public event Action<InventoryItem> OnRightMouseBtnClick;
+
+    private bool empty = true;
+
+    private void Awake()
     {
-        
+        ResetData();
+        Deselect();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResetData()
     {
-        
+        itemImage.gameObject.SetActive(false);
+        empty = true;
+    }
+
+    public void Deselect()
+    {
+        borderImage.enabled = false;
+    }
+
+    public void SetData(Sprite sprite, int quantity)
+    {
+        itemImage.gameObject.SetActive(true);
+        itemImage.sprite = sprite;
+        quantityTxt.text = quantity.ToString();
+        empty = false;
+    }
+
+    public void Select()
+    {
+        borderImage.enabled = true;
+    }
+
+    // === Unity's Event Interface methods (called automatically if no EventTrigger is used) ===
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (empty)
+            return;
+        OnItemBeginDrag?.Invoke(this);
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        OnItemDroppedOn?.Invoke(this);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        OnItemEndDrag?.Invoke(this);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightMouseBtnClick?.Invoke(this);
+        }
+        else
+        {
+            OnItemClicked?.Invoke(this);
+        }
+    }
+
+    public void OnPointerClickHandler(BaseEventData data)
+    {
+        if (empty){
+            return;
+        }
+        PointerEventData pointerData = (PointerEventData)data;
+        if (pointerData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightMouseBtnClick?.Invoke(this);
+        }
+        else
+        {
+            OnItemClicked?.Invoke(this);
+        }
+    }
+
+    public void OnDropHandler(BaseEventData data)
+    {
+        OnItemDroppedOn?.Invoke(this);
+    }
+
+    public void OnBeginDragHandler(BaseEventData data)
+    {
+        PointerEventData pointerData = (PointerEventData)data;
+        if (empty)
+            return;
+        OnItemBeginDrag?.Invoke(this);
+    }
+
+    public void OnEndDragHandler(BaseEventData data)
+    {
+        PointerEventData pointerData = (PointerEventData)data;
+        OnItemEndDrag?.Invoke(this);
     }
 }
--- The Eternal Curse/Assets/Scripts/UI/InventoryPage.cs
+++ The Eternal Curse/Assets/Scripts/UI/InventoryPage.cs
@@ -15,7 +15,6 @@ public class InventoryPage : MonoBehaviour
 
     void Start()
     {
-        // Optional: Start hidden
         Hide();
     }
 
@@ -24,9 +23,40 @@ public class InventoryPage : MonoBehaviour
         for (int i = 0; i < inventorySize; i++)
         {
             InventoryItem uiItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
-            uiItem.transform.SetParent(contentPanel, false); // false = keep local scale/pos
+            uiItem.transform.SetParent(contentPanel, false); 
             listOfItems.Add(uiItem);
+
+            uiItem.OnItemClicked += HandleItemSelection;
+            uiItem.OnItemBeginDrag += HandleBeginDrag;
+            uiItem.OnItemDroppedOn += HandleSwap;
+            uiItem.OnItemEndDrag += HandleItemEndDrag;
+            uiItem.OnRightMouseBtnClick += HandleShowItemAction;
         }
+    }
+
+    private void HandleItemSelection(InventoryItem obj)
+    {
+        Debug.Log(obj.name);
+    }
+
+    private void HandleBeginDrag(InventoryItem obj)
+    {
+        
+    }
+
+    private void HandleSwap(InventoryItem obj)
+    {
+        
+    }
+
+    private void HandleItemEndDrag(InventoryItem obj)
+    {
+        
+    }
+
+    private void HandleShowItemAction(InventoryItem obj)
+    {
+        
     }
 
     public void Show()

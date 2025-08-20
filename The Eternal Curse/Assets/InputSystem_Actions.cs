2e5c183f8c2e9fb799e9dfad06786db98864ff54
KevinHermansson <kevkevvan@gmail.com>
2025-08-20 10:40:42 +0200

    inventory UI
    i have made a ui for the inventory. it does not have any functionality except open and close on "I"

--- The Eternal Curse/Assets/InputSystem_Actions.cs
+++ The Eternal Curse/Assets/InputSystem_Actions.cs
@@ -180,6 +180,15 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": true
+                },
+                {
+                    ""name"": ""Inventory"",
+                    ""type"": ""Button"",
+                    ""id"": ""4748cfd9-780b-4171-9f5e-416458fab49a"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -576,6 +585,28 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": "";Gamepad"",
                     ""action"": ""Aim"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""76b51633-5038-45c3-b279-ddacd1132eda"",
+                    ""path"": ""<Keyboard>/i"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": "";Keyboard&Mouse"",
+                    ""action"": ""Inventory"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""2ebb01ce-4e28-46be-bac9-8284ae1335aa"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": "";Keyboard&Mouse"",
+                    ""action"": ""Inventory"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -1173,6 +1204,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         m_Player_Sprint = m_Player.FindAction("Sprint", throwIfNotFound: true);
         m_Player_FireBall = m_Player.FindAction("FireBall", throwIfNotFound: true);
         m_Player_Aim = m_Player.FindAction("Aim", throwIfNotFound: true);
+        m_Player_Inventory = m_Player.FindAction("Inventory", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -1276,6 +1308,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Sprint;
     private readonly InputAction m_Player_FireBall;
     private readonly InputAction m_Player_Aim;
+    private readonly InputAction m_Player_Inventory;
     /// <summary>
     /// Provides access to input actions defined in input action map "Player".
     /// </summary>
@@ -1327,6 +1360,10 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "Player/Aim".
         /// </summary>
         public InputAction @Aim => m_Wrapper.m_Player_Aim;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Inventory".
+        /// </summary>
+        public InputAction @Inventory => m_Wrapper.m_Player_Inventory;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -1383,6 +1420,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @Aim.started += instance.OnAim;
             @Aim.performed += instance.OnAim;
             @Aim.canceled += instance.OnAim;
+            @Inventory.started += instance.OnInventory;
+            @Inventory.performed += instance.OnInventory;
+            @Inventory.canceled += instance.OnInventory;
         }
 
         /// <summary>
@@ -1424,6 +1464,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @Aim.started -= instance.OnAim;
             @Aim.performed -= instance.OnAim;
             @Aim.canceled -= instance.OnAim;
+            @Inventory.started -= instance.OnInventory;
+            @Inventory.performed -= instance.OnInventory;
+            @Inventory.canceled -= instance.OnInventory;
         }
 
         /// <summary>
@@ -1794,6 +1837,13 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnAim(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Inventory" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnInventory(InputAction.CallbackContext context);
     }
     /// <summary>
     /// Interface to implement callback methods for all input action callbacks associated with input actions defined by "UI" which allows adding and removing callbacks.
--- The Eternal Curse/Assets/Scripts/Player/PlayerInputHandler.cs
+++ The Eternal Curse/Assets/Scripts/Player/PlayerInputHandler.cs
@@ -7,6 +7,12 @@ public class PlayerInputHandler : MonoBehaviour
     [Header("Input Actions")]
     [SerializeField] private InputActionReference fireAction;
     [SerializeField] private InputActionReference aimAction;
+    [SerializeField] private InputActionReference inventoryAction;
+    
+    [Header("Inventory UI")]
+    [SerializeField] private GameObject inventoryUI; // Assign your inventory root/panel here
+    [SerializeField] private bool pauseWhileInventoryOpen = false;
+    [SerializeField] private bool useCanvasGroupToggle = true; // Safer: does not deactivate GameObject
     
     [Header("Aiming")]
     [SerializeField] private Camera playerCamera;
@@ -15,7 +21,7 @@ public class PlayerInputHandler : MonoBehaviour
     [Header("Events")]
     public UnityEvent<Vector2> OnFirePressed;
     public UnityEvent<Vector2> OnAimChanged;
-    
+    public UnityEvent<Vector2> OnInventoryPressed;
     private Vector2 aimDirection;
     private bool isAiming = false;
     
@@ -23,6 +29,11 @@ public class PlayerInputHandler : MonoBehaviour
     {
         // Setup camera
         if (playerCamera == null) playerCamera = Camera.main;
+        // Sync inventory state with current UI active state
+        if (inventoryUI != null)
+        {
+            inventoryOpen = inventoryUI.activeSelf;
+        }
     }
     
     void OnEnable()
@@ -30,9 +41,17 @@ public class PlayerInputHandler : MonoBehaviour
         // Enable all input actions
         if (fireAction != null) fireAction.action.Enable();
         if (aimAction != null) aimAction.action.Enable();
+
+        var playerInput = GetComponent<PlayerInput>();
+        bool usingSendMessages = playerInput != null && playerInput.notificationBehavior == PlayerNotifications.SendMessages;
+        if (!usingSendMessages && inventoryAction != null)
+        {
+            inventoryAction.action.Enable();
+        }
         
         // Subscribe to events
         if (fireAction != null) fireAction.action.performed += OnFire;
+        if (!usingSendMessages && inventoryAction != null) inventoryAction.action.performed += OnInventory;
         
         Debug.Log("PlayerInputHandler enabled - Input actions should be active");
     }
@@ -41,10 +60,15 @@ public class PlayerInputHandler : MonoBehaviour
     {
         // Unsubscribe from events
         if (fireAction != null) fireAction.action.performed -= OnFire;
+
+        var playerInput = GetComponent<PlayerInput>();
+        bool usingSendMessages = playerInput != null && playerInput.notificationBehavior == PlayerNotifications.SendMessages;
+        if (!usingSendMessages && inventoryAction != null) inventoryAction.action.performed -= OnInventory;
         
         // Disable all input actions
         if (fireAction != null) fireAction.action.Disable();
         if (aimAction != null) aimAction.action.Disable();
+        if (!usingSendMessages && inventoryAction != null) inventoryAction.action.Disable();
     }
     
     void Update()
@@ -87,6 +111,62 @@ public class PlayerInputHandler : MonoBehaviour
     {
         Debug.Log("Fire button pressed! Aim direction: " + aimDirection);
         OnFirePressed?.Invoke(aimDirection);
+    }
+    
+    private bool inventoryOpen = false;
+    
+    // Called when subscribing directly to InputAction callbacks
+    private void OnInventory(InputAction.CallbackContext context)
+    {
+        ToggleInventory();
+    }
+    
+    // Called by PlayerInput when Behavior = Send Messages (InputValue variant)
+    public void OnInventory(InputValue value)
+    {
+        // Toggle only on press, ignore release to avoid double toggles
+        if (value.isPressed)
+        {
+            ToggleInventory();
+        }
+    }
+    
+    private void ToggleInventory()
+    {
+        var playerInput = GetComponent<PlayerInput>();
+        string currentMap = (playerInput != null && playerInput.currentActionMap != null) ? playerInput.currentActionMap.name : "(none)";
+        string nextState = inventoryOpen ? "Close" : "Open";
+        Debug.Log($"Inventory input received. CurrentActionMap={currentMap}. Next: {nextState} inventory.");
+        
+        inventoryOpen = !inventoryOpen;
+        if (inventoryUI != null)
+        {
+            bool wouldDisableSelf = inventoryUI == gameObject || transform.IsChildOf(inventoryUI.transform);
+            if (useCanvasGroupToggle || wouldDisableSelf)
+            {
+                var canvasGroup = inventoryUI.GetComponent<CanvasGroup>();
+                if (canvasGroup == null)
+                {
+                    canvasGroup = inventoryUI.AddComponent<CanvasGroup>();
+                }
+                // Ensure object is active when opening so CanvasGroup can show it
+                if (inventoryOpen && !inventoryUI.activeSelf)
+                {
+                    inventoryUI.SetActive(true);
+                }
+                canvasGroup.alpha = inventoryOpen ? 1f : 0f;
+                canvasGroup.interactable = inventoryOpen;
+                canvasGroup.blocksRaycasts = inventoryOpen;
+            }
+            else
+            {
+                inventoryUI.SetActive(inventoryOpen);
+            }
+        }
+        if (pauseWhileInventoryOpen)
+        {
+            Time.timeScale = inventoryOpen ? 0f : 1f;
+        }
     }
     
     // Public method to get current aim direction
